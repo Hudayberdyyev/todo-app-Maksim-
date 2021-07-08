5d640a1a0140303744c3cfd9f8149026f8a7f26a
Apfel <hudayberdyyev.ahmet@gmail.com>
2021-07-08 12:56:51 +0500

    lesson_2

--- cmd/main.go
+++ cmd/main.go
@@ -4,11 +4,13 @@ import (
 	"log"
 
 	"github.com/Hudayberdyyev/todo-app-Maksim-"
+	"github.com/Hudayberdyyev/todo-app-Maksim-/pkg/handler"
 )
 
 func main() {
+	handlers := new(handler.Handler)
 	srv := new(todo.Server)
-	if err := srv.Run("8000"); err != nil {
+	if err := srv.Run("8000", handlers.InitRoutes()); err != nil {
 		log.Fatalf("error http server: %s", err.Error())
 	}
 
