ac8abb1e1b5d05b27cc0eea6eda679272f897d6a
Apfel <hudayberdyyev.ahmet@gmail.com>
2021-07-08 14:10:14 +0500

    lesson_3

--- cmd/main.go
+++ cmd/main.go
@@ -5,10 +5,15 @@ import (
 
 	"github.com/Hudayberdyyev/todo-app-Maksim-"
 	"github.com/Hudayberdyyev/todo-app-Maksim-/pkg/handler"
+	"github.com/Hudayberdyyev/todo-app-Maksim-/pkg/repository"
+	"github.com/Hudayberdyyev/todo-app-Maksim-/pkg/service"
 )
 
 func main() {
-	handlers := new(handler.Handler)
+	// handlers := new(handler.Handler)
+	repos := repository.NewRepository()
+	services := service.NewService(repos)
+	handlers := handler.NewHandler(services)
 	srv := new(todo.Server)
 	if err := srv.Run("8000", handlers.InitRoutes()); err != nil {
 		log.Fatalf("error http server: %s", err.Error())
