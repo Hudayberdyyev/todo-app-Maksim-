f9d60749eb1ebfc60d12fd449a5a011403b1d64f
Apfel <hudayberdyyev.ahmet@gmail.com>
2021-07-09 12:10:50 +0500

    lesson4_preDB

--- cmd/main.go
+++ cmd/main.go
@@ -7,16 +7,27 @@ import (
 	"github.com/Hudayberdyyev/todo-app-Maksim-/pkg/handler"
 	"github.com/Hudayberdyyev/todo-app-Maksim-/pkg/repository"
 	"github.com/Hudayberdyyev/todo-app-Maksim-/pkg/service"
+	"github.com/spf13/viper"
 )
 
 func main() {
 	// handlers := new(handler.Handler)
+
+	if err := initConfig(); err != nil {
+		log.Fatalf("error config: %s", err.Error())
+	}
+
 	repos := repository.NewRepository()
 	services := service.NewService(repos)
 	handlers := handler.NewHandler(services)
 	srv := new(todo.Server)
-	if err := srv.Run("8000", handlers.InitRoutes()); err != nil {
+	if err := srv.Run(viper.GetString("8000"), handlers.InitRoutes()); err != nil {
 		log.Fatalf("error http server: %s", err.Error())
 	}
+}
 
+func initConfig() error {
+	viper.AddConfigPath("configs")
+	viper.SetConfigName("config")
+	return viper.ReadInConfig()
 }
