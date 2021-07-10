ec24731cbffebb89943d593a8585afffbbafee57
Apfel <hudayberdyyev.ahmet@gmail.com>
2021-07-10 19:29:21 +0500

    lesson9_part1

--- pkg/handler/list.go
+++ pkg/handler/list.go
@@ -3,11 +3,29 @@ package handler
 import (
 	"net/http"
 
+	"github.com/Hudayberdyyev/todo-app-Maksim-"
 	"github.com/gin-gonic/gin"
 )
 
 func (h *Handler) createList(c *gin.Context) {
-	id, _ := c.Get(userCtx)
+	userId, ok := getUserId(c)
+	if ok != nil {
+		return
+	}
+
+	var input todo.TodoList
+	if err := c.BindJSON(&input); err != nil {
+		newErrorResponse(c, http.StatusBadRequest, err.Error())
+		return
+	}
+
+	// go to services
+	id, err := h.services.TodoList.Create(userId, input)
+	if err != nil {
+		newErrorResponse(c, http.StatusInternalServerError, err.Error())
+		return
+	}
+
 	c.JSON(http.StatusOK, map[string]interface{}{
 		"id": id,
 	})
---
+++ pkg/service/todo_list.go
@@ -0,0 +1,20 @@
+package service
+
+import (
+	"github.com/Hudayberdyyev/todo-app-Maksim-"
+	"github.com/Hudayberdyyev/todo-app-Maksim-/pkg/repository"
+)
+
+type TodoListService struct {
+	repos repository.TodoList
+}
+
+func NewTodoListService(repos repository.TodoList) *TodoListService {
+	return &TodoListService{
+		repos: repos,
+	}
+}
+
+func (s *TodoListService) Create(userId int, list todo.TodoList) (int, error) {
+	return s.repos.Create(userId, list)
+}
